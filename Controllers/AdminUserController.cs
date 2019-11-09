0dd7a0db2d329f4c1b06da3a40047cb2cfd0dc54
trivip002 <trivip002@gmail.com>
2019-11-09 19:24:57 +0700

    - Add Update User

--- Controllers/AdminUserController.cs
+++ Controllers/AdminUserController.cs
@@ -1,4 +1,5 @@
-﻿using OnlineJewelryShopping.service.Impl;
+﻿using Newtonsoft.Json.Linq;
+using OnlineJewelryShopping.service.Impl;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,17 +13,26 @@ namespace OnlineJewelryShopping.Controllers
     {
         UserService userService = new UserService();
 
-        [HttpGet] 
+        [HttpGet]
+        [Authorize]
         [Route("api/admin/user")]
         public IEnumerable<UserRegMst> GetAllUser()
         {
             return userService.FindAll();
         }
-        [HttpGet]
+        [HttpPut]
+        [Authorize]
         [Route("api/admin/user/update")]
-        public IHttpActionResult UpdateUser()
+        public IHttpActionResult UpdateUser([FromBody]UserRegMst data)
         {
-            return Ok("aaa  ");
+            return Ok(userService.updateUser(data));
+        }
+        [HttpPost]
+        [Authorize]
+        [Route("api/admin/user/add")]
+        public IHttpActionResult AddUser([FromBody]UserRegMst data)
+        {
+            return Ok(userService.addUser(data));
         }
     }
 }
--- service/Impl/UserService.cs
+++ service/Impl/UserService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,41 @@ namespace OnlineJewelryShopping.service.Impl
     public class UserService: IUserService
     {
         OnlineJewelryShopDBEntities context = new OnlineJewelryShopDBEntities();
+
+        public string addUser(UserRegMst userRegMst)
+        {
+            try
+            {
+                userRegMst.cdate = DateTime.Now;
+                context.UserRegMsts.Add(userRegMst);
+                context.SaveChanges();
+                return "Add Success";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
         public List<UserRegMst> FindAll()
         {
             List<UserRegMst> users = context.UserRegMsts.ToList(); 
             return users;
+        }
+
+        public string updateUser(UserRegMst userRegMst)
+        {
+            try
+            {
+                context.Entry(userRegMst).State = EntityState.Modified;
+                context.SaveChanges();
+                return "update success";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+           
         }
     }
 }
