293b435d9b7b59b8521d3b8096441cc630dbfd5c
trivip002 <trivip002@gmail.com>
2019-11-09 16:47:42 +0700

    - Service commit

---
+++ Controllers/AdminUserController.cs
@@ -0,0 +1,28 @@
+﻿using OnlineJewelryShopping.service.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OnlineJewelryShopping.Controllers
+{
+    public class AdminUserController : ApiController
+    {
+        UserService userService = new UserService();
+
+        [HttpGet] 
+        [Route("api/admin/user")]
+        public IEnumerable<UserRegMst> GetAllUser()
+        {
+            return userService.FindAll();
+        }
+        [HttpGet]
+        [Route("api/admin/user/update")]
+        public IHttpActionResult UpdateUser()
+        {
+            return Ok("aaa  ");
+        }
+    }
+}
---
+++ service/Impl/UserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineJewelryShopping.service.Impl
+{
+    public class UserService: IUserService
+    {
+        OnlineJewelryShopDBEntities context = new OnlineJewelryShopDBEntities();
+        public List<UserRegMst> FindAll()
+        {
+            List<UserRegMst> users = context.UserRegMsts.ToList(); 
+            return users;
+        }
+    }
+}
